e35c8312b8915a0057a334e685e25e790b1a2b74
Adam Bricker <adam.herrala.bricker@gmail.com>
2024-02-02 13:29:11 +0200

    browser params

--- browserConf/src/App.jsx
+++ browserConf/src/App.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import {Routes, Route, useParams, useNavigate} from 'react-router-dom';
+import {Button, Header} from 'semantic-ui-react'
 
-const App = () => {
+const DefaultMessage = () => {
   return (
     <div>
-      It works!!
+      default message
+    </div>
+  )
+}
+
+const ConfirmationPage = () => {
+  const {id} = useParams();
+  return (
+    <div>
+      <div>
+        confirmation page
+      </div>
+      <div>
+        id number: {id}
+      </div>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const nagivate = useNavigate();
+
+  return (
+    <div className = 'outer-container'>
+      <div className = 'main-container'>
+        <Header as = 'h1'>Aika Email Confirmation</Header>
+        <Routes>
+          <Route path = '/' element = {<DefaultMessage />}/>
+          <Route path = '/:id' element = {<ConfirmationPage />}/>
+        </Routes>
+        <Button onClick={() => nagivate('/7')}>click</Button>
+
+      </div>
     </div>
   )
 }
