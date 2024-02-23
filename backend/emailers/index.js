d0b0e0216c2dec92f4843ef39bc7b6b2185e072a
Adam Bricker <adam.herrala.bricker@gmail.com>
2024-02-23 12:21:13 +0200

    production email conf link

--- backend/emailers/index.js
+++ backend/emailers/index.js
@@ -1,7 +1,10 @@
 const nodemailer = require('nodemailer');
 const {auth, confText} = require('./config');
+const {NODE_ENV} = require('../util/config');
 
-const baseLink = 'http://localhost:3001/email-confirmation';
+const baseLink = NODE_ENV === 'production'
+  ? 'https://aika-1l2h.onrender.com/email-confirmation'
+  : 'http://localhost:3001/email-confirmation';
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
